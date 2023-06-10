713fe0fb5184581c4b7b13cdda41b0afaaeece9d
Lindsey Wade <lindsey.wade@me.com>
2023-06-10 16:40:55 -0400

    added reviews and footer

--- app.js
+++ app.js
@@ -41,6 +41,24 @@ window.addEventListener("load", function () {
 
 
 // button onClick to services
-document.getElementById("services").onclick = function () {
-    location.href = "#services";
-};
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
